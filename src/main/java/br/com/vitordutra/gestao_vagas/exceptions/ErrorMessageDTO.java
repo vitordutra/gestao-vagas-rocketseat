624de015a1c1ae3c73c20b54d66e433489305790
João Gois <joao.gois@trustly.com>
2023-12-04 19:03:42 -0300

    Formatando mensagem de validação

---
+++ src/main/java/br/com/vitordutra/gestao_vagas/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,13 @@
+package br.com.vitordutra.gestao_vagas.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+
+  private String message;
+  private String field;
+
+}
--- src/main/java/br/com/vitordutra/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/vitordutra/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -12,14 +13,21 @@ import lombok.Data;
 public class CandidateEntity {
 
   private UUID id;
+
   private String name;
-  @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
+
+  @NotBlank()
+  @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
   private String username;
+
   @Email(message = "O campo [email] deve conter um email válido")
   private String email;
-  @Length(min = 8, max = 128)
+
+  @Length(min = 8, max = 128, message = "O campo senha deve ter no mínimo 8 e no máximo 128 caracteres")
   private String password;
+
   private String description;
+
   private String curriculum;
 
 }
