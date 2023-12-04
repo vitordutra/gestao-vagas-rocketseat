063e48e53af230fad8e89b2d8499553af99cfa6f
João Gois <joao.gois@trustly.com>
2023-12-04 09:21:07 -0300

    Inserindo Validação - [1 - Primeiros Passos] - [Gestão de Vagas]

--- src/main/java/br/com/vitordutra/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/vitordutra/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.vitordutra.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,11 @@ public class CandidateEntity {
 
   private UUID id;
   private String name;
+  @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
   private String username;
+  @Email(message = "O campo [email] deve conter um email válido")
   private String email;
+  @Length(min = 8, max = 128)
   private String password;
   private String description;
   private String curriculum;
