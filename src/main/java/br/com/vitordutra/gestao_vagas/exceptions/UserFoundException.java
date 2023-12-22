988ba5e8dcefc72174efb2ebeb1f4d09f3be61c8
João Gois <joao.gois@trustly.com>
2023-12-22 08:08:51 -0300

    Validando usuário já existente

--- src/main/java/br/com/vitordutra/gestao_vagas/exceptions/UserFoundException.java
+++ src/main/java/br/com/vitordutra/gestao_vagas/exceptions/UserFoundException.java
@@ -1,2 +1,7 @@
-package br.com.vitordutra.gestao_vagas.exceptions;public class UserFoundException {
+package br.com.vitordutra.gestao_vagas.exceptions;
+
+public class UserFoundException extends RuntimeException {
+    public UserFoundException() {
+        super("Usuário já existe");
+    }
 }
