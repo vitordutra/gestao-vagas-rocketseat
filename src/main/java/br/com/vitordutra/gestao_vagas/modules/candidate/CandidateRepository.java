ba53afef1e02b89c86a73bef5d7fe4ebe3227fa6
João Gois <joao.gois@trustly.com>
2023-12-22 07:15:41 -0300

    Criando Repositório

---
+++ src/main/java/br/com/vitordutra/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,8 @@
+package br.com.vitordutra.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+
+}
