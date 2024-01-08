dcf0f79a683dc9ba769710f85820f00bcf906336
João Gois <joao.gois@trustly.com>
2024-01-08 08:19:50 -0300

    1 - Primeiros Passos - Job Controller

--- src/main/java/br/com/vitordutra/gestao_vagas/modules/company/entities/JobEntity.java
+++ src/main/java/br/com/vitordutra/gestao_vagas/modules/company/entities/JobEntity.java
@@ -1,6 +1,7 @@
 package br.com.vitordutra.gestao_vagas.modules.company.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -10,25 +11,26 @@ import java.util.UUID;
 @Entity(name = "job")
 @Data
 public class JobEntity {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    
+
     private String description;
-    
+
     private String benefits;
-    
+
+    @NotBlank(message = "Esse campo é obrigatório")
     private String level;
-    
+
     @ManyToOne()
     @JoinColumn(name = "company_id", insertable = false, updatable = false)
     private CompanyEntity companyEntity;
-    
-    @Column(name = "company_id")
+
+    @Column(name = "company_id", nullable = false)
     private UUID companyId;
-    
+
     @CreationTimestamp
     private LocalDateTime createdAt;
-  
+
 }
