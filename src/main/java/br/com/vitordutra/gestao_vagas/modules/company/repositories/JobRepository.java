a44d498f68c9da466c62f83f8926a5f133db664e
João Gois <joao.gois@trustly.com>
2024-01-08 08:20:11 -0300

    1 - Primeiros Passos - Job Controller

---
+++ src/main/java/br/com/vitordutra/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -0,0 +1,10 @@
+package br.com.vitordutra.gestao_vagas.modules.company.repositories;
+
+import br.com.vitordutra.gestao_vagas.modules.company.entities.JobEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+  
+}
