5639e31dbae464c213806847043f996ee74eed1a
João Gois <joao.gois@trustly.com>
2024-01-13 12:34:51 -0300

    2 - Autenticação e Autorização - Refatorando JobController - Antes do DTO

--- src/main/java/br/com/vitordutra/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/vitordutra/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,14 +1,16 @@
 package br.com.vitordutra.gestao_vagas.modules.company.controllers;
 
+import br.com.vitordutra.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.vitordutra.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.vitordutra.gestao_vagas.modules.company.entities.JobEntity;
-import br.com.vitordutra.gestao_vagas.modules.company.useCases.CreateJobUseCase;
-import jakarta.validation.Valid;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/job")
@@ -18,7 +20,11 @@ public class JobController {
   private CreateJobUseCase createJobUseCase;
 
   @PostMapping("/")
-  public JobEntity create(@Valid @RequestBody JobEntity jobEntity) {
+  public JobEntity create(@Valid @RequestBody JobEntity jobEntity, HttpServletRequest request) {
+    var companyId = request.getAttribute("company_id");
+
+    jobEntity.setCompanyId(UUID.fromString(companyId.toString()));
+
     return this.createJobUseCase.execute(jobEntity);
   }
 }
