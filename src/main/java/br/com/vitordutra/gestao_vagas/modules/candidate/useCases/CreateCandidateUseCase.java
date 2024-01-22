fdeb7911a830fa508a68ca76c571091f1468edca
João Gois <joao.gois@trustly.com>
2024-01-22 11:42:08 -0300

    2 - Autenticação e Autorização - Criando Token do Candidato

--- src/main/java/br/com/vitordutra/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
+++ src/main/java/br/com/vitordutra/gestao_vagas/modules/candidate/useCases/CreateCandidateUseCase.java
@@ -4,6 +4,7 @@ import br.com.vitordutra.gestao_vagas.exceptions.UserFoundException;
 import br.com.vitordutra.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.vitordutra.gestao_vagas.modules.candidate.CandidateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,14 +12,21 @@ public class CreateCandidateUseCase {
 
     @Autowired
     private CandidateRepository candidateRepository;
-    
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public CandidateEntity execute(CandidateEntity candidateEntity) {
         this.candidateRepository
                 .findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
                 .ifPresent((user) -> {
                     throw new UserFoundException();
                 });
+
+                var password = passwordEncoder.encode(candidateEntity.getPassword());
+                candidateEntity.setPassword(password);
+
         return this.candidateRepository.save(candidateEntity);
     }
-  
+
 }
