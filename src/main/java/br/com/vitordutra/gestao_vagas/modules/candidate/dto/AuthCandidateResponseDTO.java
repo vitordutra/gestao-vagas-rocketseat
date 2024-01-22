947691f3c8c4c3c3669f2b24de91af54c1795226
João Gois <joao.gois@trustly.com>
2024-01-22 14:24:03 -0300

    2 - Autenticação e Autorização - Criando Token de Candidato

---
+++ src/main/java/br/com/vitordutra/gestao_vagas/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,16 @@
+package br.com.vitordutra.gestao_vagas.modules.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthCandidateResponseDTO {
+
+  private String access_token;
+
+}
---
+++ src/main/java/br/com/vitordutra/gestao_vagas/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,60 @@
+package br.com.vitordutra.gestao_vagas.modules.candidate.useCases;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+import javax.security.sasl.AuthenticationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import br.com.vitordutra.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.vitordutra.gestao_vagas.modules.candidate.dto.AuthCandidateRequestDTO;
+import br.com.vitordutra.gestao_vagas.modules.candidate.dto.AuthCandidateResponseDTO;
+
+@Service
+public class AuthCandidateUseCase {
+
+  @Value("${security.token.secret.candidate}")
+  private String secretKey;
+
+  @Autowired
+  private CandidateRepository candidateRepository;
+
+  @Autowired
+  private PasswordEncoder passwordEncoder;
+
+  public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO authCandidateRequestDTO) throws AuthenticationException {
+    var candidate = this.candidateRepository
+        .findByUsername(authCandidateRequestDTO.username())
+        .orElseThrow(() -> {
+          throw new UsernameNotFoundException("Invalid username or password");
+        });
+
+    var passwordMatches = this.passwordEncoder
+        .matches(authCandidateRequestDTO.password(), candidate.getPassword());
+
+    if (!passwordMatches) {
+      throw new AuthenticationException();
+    }
+
+    Algorithm algorithm = Algorithm.HMAC256(secretKey);
+    var token = JWT.create()
+        .withIssuer("javagas")
+        .withSubject(candidate.getId().toString())
+        .withClaim("roles", Arrays.asList("candidate"))
+        .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
+        .sign(algorithm);
+
+    var authCandidateResponse = AuthCandidateResponseDTO.builder()
+        .access_token(token)
+        .build();
+
+    return authCandidateResponse;
+  }
+}
