e69aa2c4d6ce5b2419d69bf09be4d4f19e738aec
João Gois <joao.gois@trustly.com>
2024-02-05 10:46:45 -0300

    4 - Testes da aplicação - Teste validando se o Job não existe

--- src/test/java/br/com/vitordutra/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
+++ src/test/java/br/com/vitordutra/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -1,6 +1,8 @@
 package br.com.vitordutra.gestao_vagas.modules.candidate.useCases;
 
+import br.com.vitordutra.gestao_vagas.exceptions.JobNotFoundException;
 import br.com.vitordutra.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.vitordutra.gestao_vagas.modules.candidate.CandidateEntity;
 import br.com.vitordutra.gestao_vagas.modules.candidate.CandidateRepository;
 import br.com.vitordutra.gestao_vagas.modules.company.repositories.JobRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ApplyJobCandidateUseCaseTest {
@@ -31,6 +36,21 @@ public class ApplyJobCandidateUseCaseTest {
             applyJobCandidateUseCase.execute(null, null);
         } catch (Exception e) {
             assertThat(e).isInstanceOf(UserNotFoundException.class);
+        }
+    }
+
+    @Test
+    @DisplayName("Should not be able to apply for a job when job not found")
+    public void shouldNotBeAbleToApplyForAJobWhenJobNotFound() {
+        var candidateId = java.util.UUID.randomUUID();
+        var candidate = new CandidateEntity();
+        candidate.setId(candidateId);
+
+        when(candidateRepository.findById(candidateId)).thenReturn(Optional.of(candidate));
+        try {
+            applyJobCandidateUseCase.execute(candidateId, null);
+        } catch (Exception e) {
+            assertThat(e).isInstanceOf(JobNotFoundException.class);
         }
     }
 }
