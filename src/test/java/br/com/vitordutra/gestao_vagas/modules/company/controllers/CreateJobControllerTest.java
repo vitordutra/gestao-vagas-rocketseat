af66f5a202a90b3aecd8ed2ec47d44e4487e7b5b
João Gois <joao.gois@trustly.com>
2024-02-16 10:53:03 -0300

    4 - Testes da aplicação - Finalizando caso de teste de sucesso

--- src/test/java/br/com/vitordutra/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/vitordutra/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -1,17 +1,25 @@
 package br.com.vitordutra.gestao_vagas.modules.company.controllers;
 
 import br.com.vitordutra.gestao_vagas.modules.company.dto.CreateJobDTO;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import br.com.vitordutra.gestao_vagas.utils.TestUtils;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.UUID;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CreateJobControllerTest {
 
     private MockMvc mvc;
@@ -19,23 +27,15 @@ public class CreateJobControllerTest {
     @Autowired
     private WebApplicationContext context;
 
-    private static String objectToJson(Object obj) {
-        try {
-            final ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     @Before
     public void setup() {
-        mvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();
     }
 
     @Test
     public void ShouldBeAbleToCreateANewJob() throws Exception {
-
 
         var createdJobDTO = CreateJobDTO.builder()
                 .benefits("BENEFITS_TEST")
@@ -43,9 +43,9 @@ public class CreateJobControllerTest {
                 .level("LEVEL_TEST")
                 .build();
 
-        var result = mvc.perform(MockMvcRequestBuilders.post("/company/job")
+        var result = mvc.perform(MockMvcRequestBuilders.post("/company/job/")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectToJson(createdJobDTO)))
+                        .content(TestUtils.objectToJson(createdJobDTO)).header("Authorization", "Bearer " + TestUtils.generateToken(UUID.randomUUID(), "JAVAGAS_@123#")))
                 .andExpect(MockMvcResultMatchers.status().isOk());
 
         System.out.println(result);
---
+++ src/test/java/br/com/vitordutra/gestao_vagas/utils/TestUtils.java
@@ -0,0 +1,36 @@
+package br.com.vitordutra.gestao_vagas.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.UUID;
+
+public class TestUtils {
+
+    public static String objectToJson(Object obj) {
+        try {
+            final ObjectMapper objectMapper = new ObjectMapper();
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String generateToken(UUID companyId, String secret) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        var expiresIn = Instant.now().plus(Duration.ofHours(2));
+
+        var token = JWT.create().withIssuer("javagas")
+                .withSubject(companyId.toString())
+                .withExpiresAt(expiresIn)
+                .withClaim("roles", Arrays.asList("COMPANY"))
+                .sign(algorithm);
+
+        return token;
+    }
+}
